a305f0825165d19472cd557d05751afca256d51b
Riquezin731 <henrique.moya.alves@escola.pr.gov.br>
2025-09-24 15:37:08 -0300

    Update utils.js
    0

--- utils.js
+++ utils.js
@@ -1,8 +1,1 @@
-export function detectCollision(a, b) {
-  return (
-    a.x < b.x + b.width &&
-    a.x + (a.width || 0) > b.x &&
-    a.y < b.y + b.height &&
-    a.y + (a.height || 0) > b.y
-  );
-}
+
