7217e647c9e45548babbef864464d23f541cb50a
Riquezin731 <henrique.moya.alves@escola.pr.gov.br>
2025-09-24 15:37:36 -0300

    Update script.js
    0

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const button = document.getElementById("activate");
-const audio = document.getElementById("rage-sound");
 
-button.addEventListener("click", () => {
-  document.body.classList.toggle("rage-mode");
-
-  if (document.body.classList.contains("rage-mode")) {
-    audio.currentTime = 0;
-    audio.play();
-    button.textContent = "DESATIVAR RAGE";
-  } else {
-    audio.pause();
-    button.textContent = "ATIVAR MODO RAGE";
-  }
-});
