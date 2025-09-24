3bc3aa16d04cb0be9280c613d7965ae638a39b7f
Riquezin731 <henrique.moya.alves@escola.pr.gov.br>
2025-09-24 15:38:45 -0300

    Update input.js
    0

--- input.js
+++ input.js
@@ -1,13 +1,1 @@
-export default class InputHandler {
-  constructor() {
-    this.keys = {};
 
-    window.addEventListener('keydown', e => {
-      this.keys[e.code] = true;
-    });
-
-    window.addEventListener('keyup', e => {
-      this.keys[e.code] = false;
-    });
-  }
-}
